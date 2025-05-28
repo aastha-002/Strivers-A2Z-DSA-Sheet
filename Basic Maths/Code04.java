d2c18b0f5ebe824c77957e62c8e590c53b2a0d07
Aastha <aasthajasuja2020@gmail.com>
2025-05-28 23:01:22 +0530

    GCD of Two Numbers

---
+++ Basic Maths/Code04.java
@@ -0,0 +1,20 @@
+// GFG - GCD of two numbers
+
+class Solution {
+    public static int gcd(int a, int b) {
+       while(a>0 && b>0){
+           if(a>b){
+               a=a%b;
+           }
+           else{
+               b=b%a;
+           }
+       }
+       if(a==0){
+           return b;
+       }
+       else{
+           return a;
+       }
+    }
+}
