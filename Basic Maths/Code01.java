3b55c2e10811e8fd1316470e0bbd1e69b9239393
Aastha <aasthajasuja2020@gmail.com>
2025-05-28 21:40:11 +0530

    Count Digits

---
+++ Basic Maths/Code01.java
@@ -0,0 +1,18 @@
+// GFG - Count Total Digits in a Number 
+
+class Solution {
+    public static int countDigits(int n) {
+        String s= n + "";
+        return s.length();
+        
+        /*int count=0;
+    
+        if(n==0){
+            return 1;
+        }
+        while(n>0){
+            n=n/10;
+            count++
+        }*/
+    }
+}
